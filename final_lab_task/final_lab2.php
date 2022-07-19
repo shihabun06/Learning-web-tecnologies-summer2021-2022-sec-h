2317339b1e5044cbd8ed8b37d2bb4516b0194537
shihabun06 <101938550+shihabun06@users.noreply.github.com>
2022-07-19 14:00:53 +0600

    Add files via upload
    WT final lab task

---
+++ final_lab_task/final_lab2.php
@@ -0,0 +1,22 @@
+<?php
+function getAllUser(){
+$conn = getconnection();
+$sql = "select * from users";
+$result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
+
+
+if($count > 0){
+    while($data = musqli_fetch_assoc($result)){
+        print_r($data);
+        echo "<br>";
+
+    }
+    else{
+        echo 'No data found!!!';
+    }
+}
+
+
+
+}
